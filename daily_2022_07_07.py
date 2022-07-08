2d7495c643139bd842fd0b8d5a6dc022b992e114
Madison Brooks <maddie@0xmaddie.page>
2022-07-07 21:15:50 -0400

    daily

---
+++ daily_2022_07_07.py
@@ -0,0 +1,179 @@
+import sys
+import math
+import cairo
+import nn_2022_07_07 as nn
+from nn_2022_07_07 import Tensor
+import numpy as np
+from loguru import logger as log
+
+tau = 2*math.pi
+DSPW = 256
+DSPH = 256
+RATE = 15
+LEN = 6
+
+display = cairo.ImageSurface(
+  cairo.FORMAT_ARGB32, DSPW, DSPH)
+img = cairo.Context(display)
+
+def set_color_space_cadet():
+  img.set_source_rgb(0.160, 0.160, 0.239)
+
+def set_color_super_pink():
+  img.set_source_rgb(0.839, 0.360, 0.678)
+
+def set_color_white():
+  img.set_source_rgb(1, 1, 1)
+
+def set_color_black():
+  img.set_source_rgb(0, 0, 0)
+
+_palette = [
+  set_color_super_pink,
+  set_color_white,
+]
+def set_color_latent(latent: Tensor):
+  point = latent[0, 3:]
+  index = np.argmax(point)
+  set_color_func = _palette[index]
+  set_color_func()
+
+def radius_latent(latent: Tensor):
+  value = latent[0, 2]
+  radius = math.exp(-abs(value))
+  return radius
+
+_frequencies = [
+  1,
+  1/3,
+  1/2,
+] 
+def clock(time: float):
+  value = [
+    math.sin(time*_frequencies[0]*tau),
+    math.cos(time*_frequencies[0]*tau),
+    math.sin(time*_frequencies[1]*tau),
+    math.cos(time*_frequencies[1]*tau),
+    math.sin(time*_frequencies[2]*tau),
+#    math.cos(time*_frequencies[2]*tau),
+  ]
+  return np.array([value])
+
+def draw_image(media, rows=64, cols=64):
+  radius = 2/rows/2
+  height = media.get_height()
+  width = media.get_width()
+  pixels = media.get_data()
+  for row in range(0, rows):
+    pct_y = row/rows
+    buf_y = math.floor(pct_y*height)
+    ndc_y = 2*pct_y-1
+    for col in range(0, cols):
+      pct_x = col/cols
+      buf_x = math.floor(pct_x*width)
+      ndc_x = 2*pct_x-1
+      blue  = pixels[buf_y*width*4+buf_x*4+0]/255.0
+      green = pixels[buf_y*width*4+buf_x*4+1]/255.0
+      red   = pixels[buf_y*width*4+buf_x*4+2]/255.0
+      alpha = pixels[buf_y*width*4+buf_x*4+3]/255.0
+      img.new_path()
+      img.set_source_rgba(red, green, blue, alpha)
+      img.rectangle(ndc_x, ndc_y, 2*radius, 2*radius)
+      img.fill()
+
+def draw_field(
+  model,
+  param: Tensor,
+  time: Tensor,
+  rows: int = 16,
+  cols: int = 16,
+):
+  max_radius = 2/rows/2
+  for row in range(0, rows):
+    dy = row/rows
+    for col in range(0, cols):
+      dx = col/cols
+      ndc_y = 2*dy-1+max_radius
+      ndc_x = 2*dx-1+max_radius
+      source = np.array([[ndc_x, ndc_y, 0, 0, 0]])
+      target = nn.apply(model, param, source+time)
+      set_color_latent(target)
+      radius = min([
+        max_radius*radius_latent(target),
+#        max_radius,
+      ])
+      img.new_path()
+      img.arc(ndc_x, ndc_y, radius, 0, tau)
+      img.fill()
+
+def draw_field_2x2(model, param, time, rows, cols):
+  img.save()
+  img.translate(-0.5, -0.5)
+  img.scale(0.5, 0.5)
+  draw_field(model, param, time, rows, cols)
+  img.restore()
+  
+  img.save()
+  img.translate(0.5, -0.5)
+  img.scale(0.5, 0.5)
+  img.scale(-1, 1)
+  draw_field(model, param, time, rows, cols)
+  img.restore()
+
+  img.save()
+  img.translate(-0.5, 0.5)
+  img.scale(0.5, 0.5)
+  img.scale(1, -1)
+  draw_field(model, param, time, rows, cols)
+  img.restore()
+
+  img.save()
+  img.translate(0.5, 0.5)
+  img.scale(0.5, 0.5)
+  img.scale(-1, -1)
+  draw_field(model, param, time, rows, cols)
+  img.restore()
+      
+rows = 8
+cols = 8
+model = nn.dense
+param = nn.init(model)
+media = cairo.ImageSurface.create_from_png(
+  'bin/daily_2022-07-01.png')
+for frame in range(0, RATE*LEN):
+  time = clock(frame/RATE)
+  set_color_black()
+  img.paint()
+  img.save()
+  img.translate(DSPW/2, DSPH/2)
+  img.scale(DSPW/2, DSPH/2)
+
+  img.save()
+  img.translate(-0.5, -0.5)
+  img.scale(0.5, 0.5)
+  draw_field_2x2(model, param, time, rows, cols)
+  img.restore()
+
+  img.save()
+  img.translate(0.5, 0.5)
+  img.scale(0.5, 0.5)
+  img.scale(-1, -1)
+  draw_field_2x2(model, param, time, rows, cols)
+  img.restore()
+
+  img.save()
+  img.translate(-0.5, 0.5)
+  img.scale(0.5, 0.5)
+  draw_image(media)
+  img.restore()
+
+  img.save()
+  img.translate(0.5, -0.5)
+  img.scale(0.5, 0.5)
+  img.scale(-1, -1)
+  draw_image(media)
+  img.restore()
+
+  img.restore()
+  pixels = display.get_data()
+  sys.stdout.buffer.write(pixels)
---
+++ nn_2022_07_07.py
@@ -0,0 +1,137 @@
+import sys
+import math
+import cairo
+import numpy as np
+from abc import ABC as Abstract
+from abc import abstractmethod
+from typing import Callable
+from loguru import logger as log
+
+_nil = np.array([[0]])
+Tensor = np.ndarray
+DIM = 2 + 1 + 2
+Exp = Callable[[Tensor], Tensor]
+
+def relu(x: Tensor) -> Tensor:
+  return np.maximum(x, 0)
+
+def normalize(x: Tensor) -> Tensor:
+  mean = np.mean(x)
+  var = np.cov(x,bias=True)
+  stddev = np.sqrt(var)
+  return (x-mean)/(stddev+1e-5)
+
+# This API is like React Hooks. A handler performs effects within a
+# dynamic scope, so you don't need to thread state through explicitly.
+_dynamic_scope = None
+
+def use_param(grade: int = 1) -> Tensor:
+  return _dynamic_scope.use_param(grade)
+
+class Scope(Abstract):
+  @abstractmethod
+  def use_param(self, grade: int) -> Tensor:
+    """
+    Return the next parameter tensor with the given grade.
+    """
+    pass
+
+class Init(Scope):
+  param_count: int = 0
+
+  def __init__(self):
+    pass
+
+  def use_param(self, grade: int) -> Tensor:
+    count = DIM**grade
+    self.param_count += count
+    return _nil
+
+class Apply(Scope):
+  param: Tensor
+  _param_index: int = 0
+
+  def __init__(self, param: Tensor):
+    self.param = param
+    self._shape_from_grade = [
+      (1, 1), (1, DIM), (DIM, DIM),
+    ]
+
+  def use_param(self, grade: int) -> Tensor:
+    param_count = DIM**grade
+    param_shape = self._shape_from_grade[grade]
+    lhs = self._param_index
+    rhs = self._param_index + param_count
+    _, total_param_count = self.param.shape
+    if rhs > total_param_count:
+      raise Exception('apply: no more parameters')
+    value = self.param[0, lhs:rhs]
+    value = np.reshape(value, param_shape)
+    self._param_index += param_count
+    return value
+
+def init(exp: Exp) -> Tensor:
+  """
+  Trace an expression for its initial parameter vector.
+  """
+  global _dynamic_scope
+  assert _dynamic_scope == None
+  _dynamic_scope = Init()
+  exp(_nil)
+  shape = (1, _dynamic_scope.param_count)
+  rng = np.random.default_rng()
+  param = rng.standard_normal(shape)
+  _dynamic_scope = None
+  return param
+
+def apply(
+  exp: Exp,
+  param: Tensor,
+  source: Tensor,
+) -> Tensor:
+  """
+  Apply an expression to a value with a parameter vector.
+  """
+  global _dynamic_scope
+  assert _dynamic_scope == None
+  _dynamic_scope = Apply(param=param)
+  target = exp(source)
+  _dynamic_scope = None
+  return target
+
+def dense(value: Tensor, depth: int = 2):
+  for _ in range(0, depth):
+    spin = use_param(grade=2)
+    shift = use_param(grade=1)
+    value = value+relu(value@spin+shift)
+    value = layer_norm(value)
+  return value
+
+def layer_norm(x: Tensor) -> Tensor:
+  g = use_param(grade=1)
+  b = use_param(grade=1)
+  return g*normalize(x)+b
+
+""" No reason to use a transformer here since we're not working on a
+"chunk".
+
+def transformer(
+  value: Tensor,
+  depth: int = 2,
+) -> Tensor:
+  for _ in range(0, depth):
+    value = value + attention(value)
+    value = layer_norm(value)
+    value = value + dense(value)
+    value = layer_norm(value)
+  return value
+
+def attention(x: Tensor) -> Tensor:
+  Q = use_param(grade=2)
+  K = use_param(grade=2)
+  V = use_param(grade=2)
+  q, k, v = x@Q, x@K, x@V
+  scale = sqrt(DIM)
+  attn = softmax((q@k.T)/scale)
+  return attn@v
+"""
